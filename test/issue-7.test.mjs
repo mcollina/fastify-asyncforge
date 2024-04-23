dd2aba1f6e9b5b7577a354228b803a4c936c7951
Matteo Collina <hello@matteocollina.com>
2024-04-23 19:36:47 +0200

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test/issue-7.test.mjs
+++ test/issue-7.test.mjs
@@ -1,6 +1,6 @@
 import { before, describe, it } from 'node:test'
 import Fastify from 'fastify'
-import { app, start } from '../index.js'
+import asyncforge, { app, start } from '../index.js'
 import assert from 'node:assert/strict'
 
 let fastify
@@ -14,9 +14,33 @@ async function build (config) {
   return server
 }
 
-describe('support exiting from a context', () => {
+async function buildWithRegister (config) {
+  const server = await Fastify()
+
+  server.register(asyncforge)
+  server.decorate('config', config)
+
+  return server
+}
+
+describe('support exiting from a context / start', () => {
   before(async () => {
     fastify = await build({ foo: 'bar' })
+  })
+
+  it('throws', () => {
+    assert.throws(app)
+  })
+
+  it('does not throw using enterWith', () => {
+    fastify.enterWith()
+    assert.equal(app(), fastify)
+  })
+})
+
+describe('support exiting from a context / register', () => {
+  before(async () => {
+    fastify = await buildWithRegister({ foo: 'bar' })
   })
 
   it('throws', () => {
